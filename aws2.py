5afeaeb4ee8dbb761eb07fa11576e7cf61dd8f22
SIDDHARTHSHARMA2 <93313929+SIDDHARTHSHARMA2@users.noreply.github.com>
2021-11-26 08:41:22 +0530

    Update aws2.py

--- aws2.py
+++ aws2.py
@@ -1,29 +1,6 @@
 import boto3
-
-
-# create a dictionary with names of databases or instances as key and their volume ids as value
-volumes_dict = {
-                  'siddharth' : 'vol-0c94b9972d85052e7',
-                
-          }
-# create a dictionary of snapshots with their snapshot ids which were created successfully
-successful_snapshots = dict()
-# iterate through each item in volumes_dict and use key as description of snapshot
-for snapshot in volumes_dict:
-    try:
-        response = client.create_snapshot(
-            Description= snapshot,
-            VolumeId= volumes_dict[snapshot],
-            DryRun= False
+  
+ec2=boto3.resource('ec2', 'us-west-2')
+response = ec2.create_snapshot(
+        VolumeId="vol-08044d6c5e1dc65c2"
         )
-        # response is a dictionary containing ResponseMetadata and SnapshotId
-        status_code = response['ResponseMetadata']['HTTPStatusCode']
-        snapshot_id = response['SnapshotId']
-        # check if status_code was 200 or not to ensure the snapshot was created successfully
-        if status_code == 200:
-            successful_snapshots[snapshot] = snapshot_id
-    except Exception as e:
-        exception_message = "There was error in creating snapshot " + snapshot + " with volume id "+volumes_dict[snapshot]+" and error is: \n"\
-                            + str(e)
-# print the snapshots which were created successfully
-print(successful_snapshots)
